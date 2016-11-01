9ec9554b84609c6005ddeb685e26d4d21c31c8d6
brady.wu <120793495@qq.com>
2016-11-01 15:18:12 +0800

    three project modules.   applications  for thinking in java.   BlogBrady  for SpringMVC REST blog,   SpringPractice for spring practice

---
+++ Applications/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Brady-Server on 2016/11/1.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World.1ddddddfdawerqr231231..");
+    }
+}
---
+++ BlogBrady/src/main/java/cn/brady/blog/controller/MainController.java
@@ -0,0 +1,16 @@
+package cn.brady.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Brady-Server on 2016/11/1.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = {"/"},method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
